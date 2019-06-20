c91d09df77280f67f2443558a49e6772bbfd3ead
Rich Sharp <richpsharp@gmail.com>
2019-06-20 00:04:11 -0500

    read it as a string?

--- raster_calculations_core.py
+++ raster_calculations_core.py
@@ -82,7 +82,7 @@ def evaluate_calculation(args, task_graph, workspace_dir):
     with open(processed_raster_list_file_path, 'r') as (
             processed_raster_list_file):
         LOGGER.debug(processed_raster_list_file)
-        processed_raster_path_list = pickle.load(
+        processed_raster_path_list = pickle.loads(
             processed_raster_list_file.read())
 
     for symbol, raster_path in zip(
