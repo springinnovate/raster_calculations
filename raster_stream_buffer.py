2450859022023fa8cf02df5357a4d31d5c4ff725
Rich Sharp <richpsharp@gmail.com>
2019-08-30 11:23:53 -0700

    modified how we search for 50m buffers, now we flow accumulate a mask

--- raster_stream_buffer.py
+++ raster_stream_buffer.py
@@ -29,9 +29,8 @@ WORKSPACE_DIR = 'raster_stream_buffer_workspace'
 
 
 def conditional_convert_op(
-        base_lulc, lulc_nodata, converted_lulc, buffer_1_array,
-        buffer_5_array, high_slope_array, high_slope_nodata,
-        stream_array, target_nodata):
+        base_lulc, lulc_nodata, converted_lulc, buffer_10m_array,
+        flow_accum_50m_slope_mask_array, stream_array, target_nodata):
     """Convert LULC to the converted one on the special cases.
 
     convert lulc to converted if:
@@ -47,8 +46,8 @@ def conditional_convert_op(
         lulc_nodata_mask |
         (numpy.isclose(high_slope_array, high_slope_nodata)))
     valid_mask = (~nodata_mask) & (
-        (buffer_1_array >= 1) | (
-            (buffer_5_array >= 1) & (high_slope_array > 0)))
+        (buffer_10m_array >= 1) | (
+            (flow_accum_50m_slope_mask_array >= 1)))
     result[valid_mask] = converted_lulc[valid_mask]
     stream_mask = (stream_array == 1) & (~nodata_mask)
     result[stream_mask] = 4
@@ -350,31 +349,6 @@ if __name__ == '__main__':
         dependent_task_list=[flow_accum_task],
         task_name='calc stream')
 
-    buffer_size_path_map = {}
-    for pixel_radius in [1, 5]:
-        kernel_filepath = os.path.join(
-            WORKSPACE_DIR, '%d_kernel.tif' % pixel_radius)
-        kernel_task = task_graph.add_task(
-            func=hat_distance_kernel,
-            args=(pixel_radius, kernel_filepath),
-            target_path_list=[kernel_filepath],
-            task_name='make kernel')
-        stream_buffer_raster = os.path.join(
-            WORKSPACE_DIR, '%d_stream_buffer.tif' % (pixel_radius))
-        buffer_size_path_map[pixel_radius] = stream_buffer_raster
-        task_graph.add_task(
-            func=pygeoprocessing.convolve_2d,
-            args=(
-                (stream_raster_path, 1), (kernel_filepath, 1),
-                stream_buffer_raster),
-            kwargs={
-                'working_dir': WORKSPACE_DIR,
-                'ignore_nodata': True,
-                },
-            dependent_task_list=[kernel_task, stream_task],
-            target_path_list=[stream_buffer_raster],
-            task_name='stream buffer %d pixels' % pixel_radius)
-
     # 1) calculate distance to channel
     distance_to_stream_path = os.path.join(
         WORKSPACE_DIR, 'distance_to_stream_in_pixels.tif')
@@ -387,53 +361,56 @@ if __name__ == '__main__':
         target_path_list=[distance_to_stream_path],
         task_name='distance to stream')
 
-    # 2) calculate weighted distance to channel with slope as distance
-    slope_sum_to_stream_path = os.path.join(
-        WORKSPACE_DIR, 'sum_of_slope_to_stream.tif')
-    slope_sum_to_stream_task = task_graph.add_task(
-        func=pygeoprocessing.routing.distance_to_channel_mfd,
-        args=(
-            (flow_direction_path, 1), (stream_raster_path, 1),
-            slope_sum_to_stream_path),
-        kwargs={
-            'weight_raster_path_band': (slope_raster_path, 1)
-        },
-        target_path_list=[slope_sum_to_stream_path],
-        dependent_task_list=[slope_task, stream_task],
-        task_name='slope sum to stream')
+    # 2) make a mask for 10m and 50m to channel
+    stream_dist_task_path_map = {}
+    buffer_nodata = -1
+    for pixel_radius in [1, 5]:
+        stream_distance_raster = os.path.join(
+            WORKSPACE_DIR, '%d_stream_buffer.tif' % (pixel_radius))
+        dist_to_stream_task = task_graph.add_task(
+            func=pygeoprocessing.symbolic.evaluate_raster_calculator_expression,
+            args=(
+                'distance_to_stream < %d' % pixel_radius,
+                {
+                    'distance_to_stream': (distance_to_stream_path, 1),
+                },
+                buffer_nodata, stream_distance_raster),
+            target_path_list=[stream_distance_raster],
+            dependent_task_list=[distance_to_stream_task],
+            task_name='stream buffer %d pixels' % pixel_radius)
+        stream_dist_task_path_map[pixel_radius] = (
+            dist_to_stream_task, stream_distance_raster)
 
-    # 3) divide weighted distance by distance -- this is the average slope
-    #    to the stream -- use that in the threshold
-    slope_to_stream_nodata = -1
-    slope_to_stream_path = os.path.join(
-        WORKSPACE_DIR, 'slope_to_stream.tif')
-    slope_to_stream_task = task_graph.add_task(
-        func=pygeoprocessing.symbolic.evaluate_raster_calculator_expression,
-        args=(
-            'sum_of_slope / distance_to_stream',
-            {
-                'sum_of_slope': slope_sum_to_stream_path,
-                'distance_to_stream': distance_to_stream_path,
-            },
-            slope_to_stream_nodata, slope_to_stream_path),
-        target_path_list=[slope_to_stream_path],
-        dependent_task_list=[
-            slope_sum_to_stream_task, distance_to_stream_task],
-        task_name='slope to stream')
-
-    average_slope_threshold = 40.0
+    # 3) make slope threshold mask
+    slope_threshold = 40.0
     slope_mask_nodata = -9999
-    steep_slope_mask_path = os.path.join(
-        WORKSPACE_DIR, 'average_steep_slope_%.2f_mask.tif' % average_slope_threshold)
-    task_graph.add_task(
+    steep_slope_50m_mask_path = os.path.join(
+        WORKSPACE_DIR,
+        'steep_slope_%.2f_in_50m_mask.tif' % slope_threshold)
+    steep_slope_50m_task = task_graph.add_task(
         func=pygeoprocessing.symbolic.evaluate_raster_calculator_expression,
         args=(
-            'slope > %f' % average_slope_threshold,
-            {'slope': (slope_to_stream_path, 1)}, slope_mask_nodata,
-            steep_slope_mask_path),
-        target_path_list=[steep_slope_mask_path],
-        dependent_task_list=[slope_to_stream_task],
-        task_name='mask slope')
+            '(slope > %f) * (distance_to_stream < %d)' % (
+                slope_threshold, 5),
+            {'slope': (slope_raster_path, 1),
+             'distance_to_stream': (stream_dist_task_path_map[5][1], 1)},
+            slope_mask_nodata,
+            steep_slope_50m_mask_path),
+        target_path_list=[steep_slope_50m_mask_path],
+        dependent_task_list=[slope_task, stream_dist_task_path_map[5][0]],
+        task_name='mask slope to 40%')
+
+    # 4) weighted flow accum of slope threshold mask
+    flow_accum_slope_mask_path = os.path.join(
+        WORKSPACE_DIR, 'flow_accum_masked_high_slope.tif')
+    slope_flow_accum_task = task_graph.add_task(
+        func=pygeoprocessing.routing.flow_accumulation_mfd,
+        args=(
+            (flow_direction_path, 1), flow_accum_slope_mask_path),
+        kwargs={'weight_raster_path_band': steep_slope_50m_mask_path},
+        target_path_list=[flow_accum_slope_mask_path],
+        dependent_task_list=[steep_slope_50m_task, flow_dir_task],
+        task_name='masked slope weighted flow accum')
 
     lulc_to_converted_map = {
         0: 100,
@@ -483,9 +460,8 @@ if __name__ == '__main__':
         args=(
             ((aligned_raster_path_list[1], 1), (base_lulc_nodata, 'raw'),
              (potential_converted_landover_raster_path, 1),
-             (buffer_size_path_map[1], 1), (buffer_size_path_map[5], 1),
-             (steep_slope_mask_path, 1), (slope_mask_nodata, 'raw'),
-             (stream_raster_path, 1),
+             (stream_dist_task_path_map[1][1], 1),
+             (flow_accum_slope_mask_path, 1), (stream_raster_path, 1),
              (target_lulc_nodata, 'raw')),
             conditional_convert_op, converted_landover_raster_path,
             gdal.GDT_Int16, target_lulc_nodata),
