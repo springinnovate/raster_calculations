0ffd9f4700ac9d7f4a41c5535899d915ef26965c
Rich Sharp <richpsharp@gmail.com>
2019-09-23 19:28:44 -0500

    getting the final raster built for pasting in

--- normalize_by_geometry.py
+++ normalize_by_geometry.py
@@ -13,6 +13,7 @@ import logging
 
 import shapely.wkb
 from osgeo import gdal
+from osgeo import ogr
 import taskgraph
 import pygeoprocessing
 
@@ -26,7 +27,8 @@ LOGGER = logging.getLogger(__name__)
 
 
 def normalize_by_polygon(
-        raster_path, vector_path, percentile, clamp_range, target_path):
+        raster_path, vector_path, percentile, clamp_range, workspace_dir,
+        target_path):
     """Normalize a raster locally by regions defined by vector.
 
     Parameters:
@@ -37,20 +39,25 @@ def normalize_by_polygon(
         percentile (float): a number in the range [0, 100] that is used to
             normalize the local regions defined by `vector_path`. This number
             will be used to calculate the percentile in each region separately
+        clamp_range (list or tuple): a min/max range to clamp the normalized
+            result by.
+        workspace_dir (str): path to a workspace to create and keep
+            intermediate files.
 
     Returns:
         None.
 
     """
     base_dir = os.path.dirname(target_path)
-    try:
-        os.makedirs(base_dir)
-    except OSError:
-        pass
+    for dir_path in [base_dir, workspace_dir]:
+        try:
+            os.makedirs(dir_path)
+        except OSError:
+            pass
 
-    workspace_dir = tempfile.mkdtemp(dir=base_dir)
-    vector = gdal.OpenEx(vector_path, gdal.OF_VECTOR)
+    vector = ogr.Open(vector_path)
     layer = vector.GetLayer()
+    fid_to_percentile_pickle_path = {}
     for feature in layer:
         # clip the original layer and then mask it
         fid = feature.GetFID()
@@ -61,19 +68,42 @@ def normalize_by_polygon(
             args=(raster_path, vector_path, fid, feature_mask_path),
             target_path_list=[feature_mask_path],
             task_name='mask feature %d' % fid)
+        percentile_pickle_path = os.path.join(
+            workspace_dir, '%s_%d.pickle' % (
+                os.path.splitext(os.path.basename(raster_path))[0],
+                percentile))
+        _ = TASK_GRAPH.add_task(
+            func=calculate_percentile,
+            args=(feature_mask_path, [percentile], base_dir,
+                  percentile_pickle_path),
+            target_path_list=[percentile_pickle_path],
+            dependent_task_list=[mask_raster_task],
+            task_name='calculating %s' % percentile_pickle_path)
+        fid_to_percentile_pickle_path[fid] = percentile_pickle_path
+        feature = None
+
+    local_vector_path = os.path.join(workspace_dir, 'local_vector.gpkg')
+    gpkg_driver = ogr.GetDriverByName('GPKG')
+    local_vector = gpkg_driver.CopyDataSource(vector, local_vector_path)
+    vector = None
+    layer = None
+    local_layer = local_vector.GetLayer()
+    local_layer.CreateField(ogr.FieldDefn('norm_val', ogr.OFTReal))
+
+    global_norm_value_raster_path = os.path.join(
+        workspace_dir, 'global_norm_values.tif')
+    pygeoprocessing.new_raster_from_base(
+        raster_path, global_norm_value_raster_path, gdal.GDT_Float32,
+        [-1], raster_driver_creation_tuple=(
+            'GTIFF', ('TILED=YES', 'BIGTIFF=YES', 'COMPRESS=ZSTD',
+            'BLOCKXSIZE=256', 'BLOCKYSIZE=256', 'SPARSE_OK=TRUE')))
 
     TASK_GRAPH.join()
-    percentile_pickle_path = '%s_%d.pickle' % (
-        os.path.splitext(os.path.basename(raster_path))[0], percentile)
-    calculate_percentile_task = TASK_GRAPH.add_task(
-        func=calculate_percentile,
-        args=(raster_path, [percentile], base_dir, percentile_pickle_path),
-        target_path_list=[percentile_pickle_path],
-        task_name='calculating %s' % percentile_pickle_path)
-    calculate_percentile_task.join()
-    with open(percentile_pickle_path, 'wb') as pickle_file:
-        percentile_values_list = pickle.load(pickle_file)
-    LOGGER.debug(percentile_values_list)
+    for fid, pickle_path in fid_to_percentile_pickle_path.items():
+        feature = local_layer.GetFeature(fid)
+        with open(pickle_path, 'rb') as pickle_file:
+            percentile_list = pickle.load(pickle_file)
+        feature.SetField('norm_val', percentile_list[0])
 
 
 def clip_and_mask_raster(
@@ -141,9 +171,10 @@ def calculate_percentile(
 
 
 if __name__ == '__main__':
-    TASK_GRAPH = taskgraph.TaskGraph('.', -1)
+    TASK_GRAPH = taskgraph.TaskGraph('.', 0)
     RASTER_PATH = 'local_data/potential_pollination_edge_md5_3b0171d8dac47d2aa2c6f41fb94b6243.tif'
     VECTOR_PATH = 'local_data/TM_WORLD_BORDERS_SIMPL-0.3_md5_c0d1b65f6986609031e4d26c6c257f07.gpkg'
     TARGET_PATH = 'normalize_workspace/normalized_by_country.tif'
     normalize_by_polygon(
-        RASTER_PATH, VECTOR_PATH, 97, [0, 1], TARGET_PATH)
+        RASTER_PATH, VECTOR_PATH, 97, [0, 1], 'normalize_workspace/churn',
+        TARGET_PATH)
