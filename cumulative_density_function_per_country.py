5306f54eec72e2b4e2bb8502779985fe656b7569
becky <rchaplinkramer@gmail.com>
2020-02-20 12:23:24 -0800

    looping through a bucket of rasters

--- cumulative_density_function_per_country.py
+++ cumulative_density_function_per_country.py
@@ -4,6 +4,7 @@ import sys
 import os
 import logging
 import ecoshard
+import subprocess
 
 import matplotlib.pyplot
 import numpy
@@ -16,14 +17,17 @@ import taskgraph
 
 gdal.SetCacheMax(2**30)
 
-RASTER_PATH = r"C:\Users\Becky\Documents\raster_calculations\normalized_realized_grazing_md5_d03b_resample_30x.tif"
-#RASTER_PATH = r"C:\Users\rpsharp\Downloads\normalized_realized_grazing_md5_d03b584dac965539a77bf96cba3f8096.tif"
+BUCKET_PATTERN = 'gs://shared-with-users/realized_services/*.tif'
+#raster_path = r"C:\Users\Becky\Documents\raster_calculations\normalized_realized_grazing_md5_d03b_resample_30x.tif"
+#raster_path = r"C:\Users\rpsharp\Downloads\normalized_realized_grazing_md5_d03b584dac965539a77bf96cba3f8096.tif"
 WORKSPACE_DIR = 'cdf_by_country'
+CHURN_DIR = os.path.join(WORKSPACE_DIR, 'churn')
 NCPUS = -1
-try:
-    os.makedirs(WORKSPACE_DIR)
-except OSError:
-    pass
+for dir_path in [WORKSPACE_DIR, CHURN_DIR]:
+    try:
+        os.makedirs(dir_path)
+    except OSError:
+        pass
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -65,93 +69,100 @@ def main():
     wgs84_srs = osr.SpatialReference()
     wgs84_srs.ImportFromEPSG(4326)
 
-    raster_info = pygeoprocessing.get_raster_info(RASTER_PATH)
+    result = subprocess.run('gsutil ls %s' % BUCKET_PATTERN, capture_output=True, shell=True, check=True)
+    for gs_path in [x.decode('utf-8') for x in result.stdout.splitlines()]:
+        print(gs_path)
+        raster_path = os.path.join(CHURN_DIR, os.path.basename(gs_path))
+        print('copying %s to %s' % (gs_path, raster_path))
+        subprocess.run('gsutil cp %s %s' % (gs_path, raster_path), shell=True, check=True)
+        raster_info = pygeoprocessing.get_raster_info(raster_path)
+        break
 
-    country_threshold_table_path = os.path.join(
-        WORKSPACE_DIR, 'country_threshold.csv')
-    country_threshold_table_file = open(country_threshold_table_path, 'w')
-    country_threshold_table_file.write('country,percentile at 90% max,pixel count\n')
-    for world_border_feature in world_borders_layer:
-        country_name = world_border_feature.GetField('NAME')
-        LOGGER.debug(country_name)
-        country_workspace = os.path.join(COUNTRY_WORKSPACES, country_name)
-        try:
-            os.makedirs(country_workspace)
-        except OSError:
-            pass
+        country_threshold_table_path = os.path.join(
+            WORKSPACE_DIR, 'country_threshold.csv')
+        country_threshold_table_file = open(country_threshold_table_path, 'w')
+        country_threshold_table_file.write('country,percentile at 90% max,pixel count\n')
+        for world_border_feature in world_borders_layer:
+            country_name = world_border_feature.GetField('NAME')
+            LOGGER.debug(country_name)
+            country_workspace = os.path.join(COUNTRY_WORKSPACES, country_name)
+            try:
+                os.makedirs(country_workspace)
+            except OSError:
+                pass
 
-        country_vector = os.path.join(
-            country_workspace, '%s.gpkg' % country_name)
-        country_vector_complete_token = os.path.join(
-            country_workspace, '%s.COMPLETE' % country_name)
-        extract_feature(
-            world_borders_path, world_border_feature.GetFID(),
-            wgs84_srs.ExportToWkt(), country_vector,
-            country_vector_complete_token)
+            country_vector = os.path.join(
+                country_workspace, '%s.gpkg' % country_name)
+            country_vector_complete_token = os.path.join(
+                country_workspace, '%s.COMPLETE' % country_name)
+            extract_feature(
+                world_borders_path, world_border_feature.GetFID(),
+                wgs84_srs.ExportToWkt(), country_vector,
+                country_vector_complete_token)
 
-        country_raster_path = os.path.join(country_workspace, '%s_%s' % (
-            country_name, os.path.basename(RASTER_PATH)))
+            country_raster_path = os.path.join(country_workspace, '%s_%s' % (
+                country_name, os.path.basename(raster_path)))
 
-        country_vector_info = pygeoprocessing.get_vector_info(country_vector)
-        pygeoprocessing.warp_raster(
-            RASTER_PATH, raster_info['pixel_size'], country_raster_path,
-            'near', target_bb=country_vector_info['bounding_box'],
-            vector_mask_options={'mask_vector_path': country_vector},
-            working_dir=country_workspace)
+            country_vector_info = pygeoprocessing.get_vector_info(country_vector)
+            pygeoprocessing.warp_raster(
+                raster_path, raster_info['pixel_size'], country_raster_path,
+                'near', target_bb=country_vector_info['bounding_box'],
+                vector_mask_options={'mask_vector_path': country_vector},
+                working_dir=country_workspace)
 
-        percentile_values = pygeoprocessing.raster_band_percentile(
-            (country_raster_path, 1), country_workspace, PERCENTILE_LIST)
-        if len(percentile_values) != len(PERCENTILE_LIST):
-            continue
-        LOGGER.debug(
-            "len percentile_values: %d len PERCENTILE_LIST: %d",
-            len(percentile_values), len(PERCENTILE_LIST))
+            percentile_values = pygeoprocessing.raster_band_percentile(
+                (country_raster_path, 1), country_workspace, PERCENTILE_LIST)
+            if len(percentile_values) != len(PERCENTILE_LIST):
+                continue
+            LOGGER.debug(
+                "len percentile_values: %d len PERCENTILE_LIST: %d",
+                len(percentile_values), len(PERCENTILE_LIST))
 
-        cdf_array = [0.0] * len(percentile_values)
+            cdf_array = [0.0] * len(percentile_values)
 
-        nodata = pygeoprocessing.get_raster_info(
-            country_raster_path)['nodata'][0]
-        pixel_count = 0
-        for _, data_block in pygeoprocessing.iterblocks(
-                (country_raster_path, 1)):
-            nodata_mask = ~numpy.isclose(data_block, nodata)
-            pixel_count += numpy.count_nonzero(nodata_mask)
-            for index, percentile_value in enumerate(percentile_values):
-                cdf_array[index] += numpy.sum(data_block[
-                    nodata_mask & (data_block >= percentile_value)])
+            nodata = pygeoprocessing.get_raster_info(
+                country_raster_path)['nodata'][0]
+            pixel_count = 0
+            for _, data_block in pygeoprocessing.iterblocks(
+                    (country_raster_path, 1)):
+                nodata_mask = ~numpy.isclose(data_block, nodata)
+                pixel_count += numpy.count_nonzero(nodata_mask)
+                for index, percentile_value in enumerate(percentile_values):
+                    cdf_array[index] += numpy.sum(data_block[
+                        nodata_mask & (data_block >= percentile_value)])
 
-        # threshold is at 90% says Becky
-        threshold_limit = 0.9 * cdf_array[2]
+            # threshold is at 90% says Becky
+            threshold_limit = 0.9 * cdf_array[2]
 
-        LOGGER.debug(cdf_array)
-        fig, ax = matplotlib.pyplot.subplots()
-        ax.plot(list(reversed(PERCENTILE_LIST)), cdf_array)
-        f = scipy.interpolate.interp1d(
-            cdf_array, list(reversed(PERCENTILE_LIST)))
-        try:
-            cdf_threshold = f(threshold_limit)
-        except ValueError:
-            LOGGER.exception(
-                "error when passing threshold_limit: %s\ncdf_array: %s" % (
-                    threshold_limit, cdf_array))
-            cdf_threshold = cdf_array[2]
+            LOGGER.debug(cdf_array)
+            fig, ax = matplotlib.pyplot.subplots()
+            ax.plot(list(reversed(PERCENTILE_LIST)), cdf_array)
+            f = scipy.interpolate.interp1d(
+                cdf_array, list(reversed(PERCENTILE_LIST)))
+            try:
+                cdf_threshold = f(threshold_limit)
+            except ValueError:
+                LOGGER.exception(
+                    "error when passing threshold_limit: %s\ncdf_array: %s" % (
+                        threshold_limit, cdf_array))
+                cdf_threshold = cdf_array[2]
 
-        ax.plot([0, 100], [threshold_limit, threshold_limit], 'k:', linewidth=2)
-        ax.plot([cdf_threshold, cdf_threshold], [cdf_array[0], cdf_array[-1]], 'k:', linewidth=2)
+            ax.plot([0, 100], [threshold_limit, threshold_limit], 'k:', linewidth=2)
+            ax.plot([cdf_threshold, cdf_threshold], [cdf_array[0], cdf_array[-1]], 'k:', linewidth=2)
 
-        ax.grid(True, linestyle='-.')
-        ax.set_title(
-            '%s CDF. 90%% max at %.2f and %.2f%%\nn=%d' % (country_name, threshold_limit, cdf_threshold, pixel_count))
-        ax.set_ylabel('Sum of %s up to 100-percentile' % os.path.basename(RASTER_PATH))
-        ax.set_ylabel('100-percentile')
-        ax.tick_params(labelcolor='r', labelsize='medium', width=3)
-        matplotlib.pyplot.autoscale(enable=True, tight=True)
-        matplotlib.pyplot.savefig(
-            os.path.join(COUNTRY_WORKSPACES, '%s_cdf.png' % country_name))
-        country_threshold_table_file.write(
-            '%s, %f, %d\n' % (country_name, cdf_threshold, pixel_count))
-        country_threshold_table_file.flush()
-    country_threshold_table_file.close()
+            ax.grid(True, linestyle='-.')
+            ax.set_title(
+                '%s CDF. 90%% max at %.2f and %.2f%%\nn=%d' % (country_name, threshold_limit, cdf_threshold, pixel_count))
+            ax.set_ylabel('Sum of %s up to 100-percentile' % os.path.basename(raster_path))
+            ax.set_ylabel('100-percentile')
+            ax.tick_params(labelcolor='r', labelsize='medium', width=3)
+            matplotlib.pyplot.autoscale(enable=True, tight=True)
+            matplotlib.pyplot.savefig(
+                os.path.join(COUNTRY_WORKSPACES, '%s_cdf.png' % country_name))
+            country_threshold_table_file.write(
+                '%s, %f, %d\n' % (country_name, cdf_threshold, pixel_count))
+            country_threshold_table_file.flush()
+        country_threshold_table_file.close()
 
 
 def extract_feature(
