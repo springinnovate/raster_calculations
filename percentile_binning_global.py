aad6ba9702907f15b85200afe3c65fe2675d2014
Rich Sharp <richpsharp@gmail.com>
2020-02-21 22:18:40 -0800

    matching the csvs with the tifs

---
+++ percentile_binning_global.py
@@ -0,0 +1,15 @@
+"""Mask global rasters to have integer bins rather than values."""
+import glob
+import os
+import re
+
+if __name__ == '__main__':
+    for raster_path in glob.glob('realized_service/*.tif'):
+        dirname = os.path.dirname(raster_path)
+        basename = os.path.basename(raster_path)
+        base_without_hash = re.match('(.*)_md5*', basename).group(1)
+        print(dirname, base_without_hash)
+        for table_path in glob.glob(os.path.join(
+            dirname, 'tables', '%s*.csv' % os.path.splitext(
+                base_without_hash)[0])):
+            print('********* %s' % table_path)
