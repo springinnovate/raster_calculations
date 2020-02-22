be1685d216124f5ea979840c7cdafd69402ff437
Rich Sharp <richpsharp@gmail.com>
2020-02-22 09:29:16 -0800

    getting the tenth percentile cutoffs

--- percentile_binning_global.py
+++ percentile_binning_global.py
@@ -5,6 +5,15 @@ import re
 
 import pandas
 
+PERCENTILES = [
+    0, 0.01, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70,
+    75, 80, 85, 90, 95, 96, 97, 98, 99, 99.9, 100]
+
+TENTH_INDEXES = [PERCENTILES.index(x) for x in range(10, 101, 10)]
+
+BIN_INTEGERS = [1 + int(x) // 10 for x in PERCENTILES]
+print(TENTH_INDEXES)
+
 if __name__ == '__main__':
     for raster_path in glob.glob('realized_service/*.tif'):
         dirname = os.path.dirname(raster_path)
@@ -14,5 +23,8 @@ if __name__ == '__main__':
         for table_path in glob.glob(os.path.join(
             dirname, 'tables', '%s*.csv' % os.path.splitext(
                 base_without_hash)[0])):
-            frame = pandas.read_csv(table_path).iloc[0]
-            print(frame)
+            frame = pandas.read_csv(table_path)
+            cutoff_percentile_values = []
+            for index in TENTH_INDEXES:
+                cutoff_percentile_values.append(frame.iloc[0, 2+index])
+        print(cutoff_percentile_values)
