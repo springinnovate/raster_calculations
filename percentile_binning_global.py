6b87b17e9eb242296dce17345b1fb75a1e23cd24
Rich Sharp <richpsharp@gmail.com>
2020-02-22 09:48:12 -0800

    bin percntiles

--- percentile_binning_global.py
+++ percentile_binning_global.py
@@ -3,7 +3,10 @@ import glob
 import os
 import re
 
+from osgeo import gdal
 import pandas
+import pygeoprocessing
+import numpy
 
 PERCENTILES = [
     0, 0.01, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70,
@@ -12,7 +15,28 @@ PERCENTILES = [
 TENTH_INDEXES = [PERCENTILES.index(x) for x in range(10, 101, 10)]
 
 BIN_INTEGERS = [1 + int(x) // 10 for x in PERCENTILES]
-print(TENTH_INDEXES)
+
+BIN_WORKSPACE = 'bin_rasters'
+try:
+    os.makedirs(BIN_WORKSPACE)
+except OSError:
+    pass
+
+
+def mask_to_percentile(
+        base_array, base_nodata, target_nodata, cutoff_percentile_values):
+    result = numpy.zeros(base_array.shape, dtype=numpy.int8)
+    zero_mask = numpy.isclose(base_array, 0.0)
+    nodata_mask = numpy.isclose(base_array, base_nodata)
+    result[nodata_mask] = target_nodata
+    for cutoff_index in range(len(cutoff_percentile_values)-1):
+        bin_value = cutoff_index + 1
+        bin_mask = ~zero_mask & (
+            (base_array >= cutoff_percentile_values[cutoff_index]) &
+            (base_array < cutoff_percentile_values[cutoff_index+1]))
+        result[bin_mask] = bin_value
+    return result
+
 
 if __name__ == '__main__':
     for raster_path in glob.glob('realized_service/*.tif'):
@@ -25,6 +49,15 @@ if __name__ == '__main__':
                 base_without_hash)[0])):
             frame = pandas.read_csv(table_path)
             cutoff_percentile_values = []
-            for index in TENTH_INDEXES:
-                cutoff_percentile_values.append(frame.iloc[0, 2+index])
+            for index, tenth_index in enumerate(TENTH_INDEXES):
+                cutoff_percentile_values.append(frame.iloc[0, 2+tenth_index])
+            break
         print(cutoff_percentile_values)
+        bin_raster = os.path.join(
+            BIN_WORKSPACE, '%s_bins.tif' % os.path.splitext(
+                os.path.basename(raster_path))[0])
+        nodata = pygeoprocessing.get_raster_info(raster_path)['nodata'][0]
+        pygeoprocessing.raster_calculator(
+            [(raster_path, 1), (nodata, 'raw'), (11, 'raw'),
+             (cutoff_percentile_values, 'raw')], mask_to_percentile,
+            bin_raster, gdal.GDT_Byte, 11)
