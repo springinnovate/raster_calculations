8200c3c7334b8dade9eb67fa8b689742ec71c674
Rich Sharp <richpsharp@gmail.com>
2020-03-09 17:57:43 -0700

    parameters out of order

--- cumulative_density_function_per_country.py
+++ cumulative_density_function_per_country.py
@@ -544,21 +544,22 @@ def main():
         result = _execute_sqlite(
             '''
             SELECT
-              percentile_list, percentile0_list, cdf, cdfnodata0, country_id
+              country_id, percentile_list, percentile0_list, cdf, cdfnodata0
             FROM job_status
             WHERE raster_id=?;
             ''',
             WORK_DATABASE_PATH, execute='execute', argument_list=[raster_id],
             fetch='all')
-        percentile_map = {}
-        for (country_id, percentile_list, percentile0_list, cdf, cdfnodata0) \
-                in result:
-            LOGGER.debug('%s, %s, %s, %s, %s', (country_id, percentile_list, percentile0_list, cdf, cdfnodata0))
-            percentile_map[country_id] = (
+
+        percentile_map = {
+            country_id: (
                 pickle.loads(percentile_list),
                 pickle.loads(percentile0_list),
                 pickle.loads(cdf),
                 pickle.loads(cdfnodata0))
+            for (country_id, percentile_list, percentile0_list,
+                 cdf, cdfnodata0) in result
+        }
         LOGGER.debug('percentile map: %s', percentile_map)
         global_available = False
         if None in percentile_map:
