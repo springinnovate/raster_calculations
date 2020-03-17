f8bf643dae27e16f3367f70975a1f3bb1ae4a69c
Rich Sharp <richpsharp@gmail.com>
2020-03-17 13:09:07 -0700

    it's a feature id now

--- country_percentile_cdf_database_querier.py
+++ country_percentile_cdf_database_querier.py
@@ -16,7 +16,7 @@ def main():
         SELECT
           percentile_list, percentile0_list, cdf, cdfnodata0
         FROM job_status
-        WHERE raster_id=? AND country_id=?;
+        WHERE raster_id=? AND feature_id=?;
         ''',
         WORK_DATABASE_PATH, execute='execute', argument_list=[
             'realized_grazing_natnotforest', 'CHN'],
