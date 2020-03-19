ea65ad9c95f967746bd8612188a11edb95df3931
Rich Sharp <richpsharp@gmail.com>
2020-03-19 10:32:16 -0700

    comment out everything but test case

--- cumulative_density_function_per_country.py
+++ cumulative_density_function_per_country.py
@@ -50,8 +50,9 @@ WORK_MAP = {
             'countries_iso3_md5_6fb2431e911401992e6e56ddf0a9bcda.gpkg'),
         'fieldname_id': 'iso3',
         'raster_gs_pattern_list': [
-            'gs://shared-with-users/realized_services/terrestrial/*.tif',
-            'gs://shared-with-users/realized_services/terrestrial/new/*.tif'
+            'gs://shared-with-users/realized_services/small_test/*.tif',
+            # 'gs://shared-with-users/realized_services/terrestrial/*.tif',
+            # 'gs://shared-with-users/realized_services/terrestrial/new/*.tif'
             ]
     },
     'eez': {
@@ -61,7 +62,8 @@ WORK_MAP = {
             'eez_v11_md5_72307ea605d6712bf79618f33e67676e.gpkg'),
         'fieldname_id': 'ISO_SOV1',
         'raster_gs_pattern_list': [
-            'gs://shared-with-users/realized_services/marine/*.tif'
+            'gs://shared-with-users/realized_services/small_test/*.tif',
+            # 'gs://shared-with-users/realized_services/marine/*.tif'
             ]
     }
 }
