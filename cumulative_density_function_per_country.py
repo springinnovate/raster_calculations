cb7863eb30534877fb5ae57f31971172816657ba
Rich Sharp <richpsharp@gmail.com>
2020-03-19 18:04:50 -0700

    attempt to fix autoproxy

--- cumulative_density_function_per_country.py
+++ cumulative_density_function_per_country.py
@@ -26,6 +26,21 @@ import taskgraph
 gdal.SetCacheMax(2**30)
 gdal.UseExceptions()
 
+# Backup original AutoProxy function
+backup_autoproxy = multiprocessing.managers.AutoProxy
+
+
+# Defining a new AutoProxy that handles unwanted key argument 'manager_owned'
+def redefined_autoproxy(token, serializer, manager=None, authkey=None,
+                        exposed=None, incref=True, manager_owned=True):
+    # Calling original AutoProxy without the unwanted key argument
+    return backup_autoproxy(
+        token, serializer, manager, authkey, exposed, incref)
+
+
+# Updating AutoProxy definition in multiprocessing.managers package
+multiprocessing.managers.AutoProxy = redefined_autoproxy
+
 WORKSPACE_DIR = 'cdf_by_country'
 ECOSHARD_DIR = os.path.join(WORKSPACE_DIR, 'ecoshard')
 CHURN_DIR = os.path.join(WORKSPACE_DIR, 'churn')
