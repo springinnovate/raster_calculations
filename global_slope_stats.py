842f9570d3b241383b673c7c70f31463add06c4c
Rich Sharp <richpsharp@gmail.com>
2020-03-31 16:17:48 -0700

    entry point

---
+++ global_slope_stats.py
@@ -0,0 +1,14 @@
+"""Global slope stats."""
+
+GLOBAL_DEM_ECOSHARD_URL = (
+    'https://storage.googleapis.com/nci-ecoshards/'
+    'global_dem_3s_blake2b_0532bf0a1bedbe5a98d1dc449a33ef0c.zip')
+
+
+def main():
+    """Main."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
