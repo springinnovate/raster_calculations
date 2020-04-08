2e3d155a18170cef4baa196c57ffc46cedc0d8dd
Rich Sharp <richpsharp@gmail.com>
2020-04-08 00:12:30 -0700

    writing results in parallel

--- random_sampling_raster_analysis.py
+++ random_sampling_raster_analysis.py
@@ -3,6 +3,7 @@ import logging
 import os
 import multiprocessing
 import sys
+import time
 
 from osgeo import gdal
 from osgeo import ogr
@@ -101,58 +102,16 @@ def robs_function(
     return result
 
 
-if __name__ == "__main__":
-    for dir_path in [WORKSPACE_DIR, CHURN_DIR]:
-        try:
-            os.makedirs(dir_path)
-        except OSError:
-            pass
+def write_results(
+        result_queue, feature_count, base_vector_path, point_vector_path):
+    vector = gdal.OpenEx(base_vector_path, gdal.OF_VECTOR)
+    poly_layer = vector.GetLayer()
+    layer_defn = poly_layer.GetLayerDefn()
 
-    task_graph = taskgraph.TaskGraph(CHURN_DIR, -1)
+    point_vector = gdal.OpenEx(
+        point_vector_path, gdal.OF_VECTOR | gdal.GA_Update)
+    point_layer = point_vector.GetLayer()
 
-    vector_path = 'data/Exposure4OLU2.shp'
-    raster_path = 'data/olu_00000-0-0-000-000000-00000-00-00000-00_depth.tif'
-    raster_info = pygeoprocessing.get_raster_info(raster_path)
-    raster_projection = raster_info['projection']
-    base_nodata = raster_info['nodata'][0]
-
-    reprojected_vector_path = os.path.join(
-        CHURN_DIR, 'reprojected_' + os.path.basename(vector_path))
-
-    reproject_task = task_graph.add_task(
-        func=pygeoprocessing.reproject_vector,
-        args=(vector_path, raster_projection, reprojected_vector_path),
-        target_path_list=[reprojected_vector_path],
-        task_name='project vector to raster')
-
-    fields_to_process = [
-        'RES1', 'RES2', 'RES3A', 'RES3B', 'RES3C', 'RES3D', 'RES3E', 'RES3F',
-        'RES4', 'RES5', 'RES6', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6',
-        'COM7', 'COM8', 'COM9', 'COM10', 'IND1', 'IND2', 'IND3', 'IND4',
-        'IND5', 'IND6', 'AGR1', 'REL1', 'GOV1', 'GOV2', 'EDU1', 'EDU2']
-
-    gpkg_driver = ogr.GetDriverByName('GPKG')
-    point_vector_path = os.path.join(CHURN_DIR, 'point_sample.gpkg')
-    point_vector = gpkg_driver.CreateDataSource(
-        point_vector_path)
-    projection_srs = osr.SpatialReference()
-    projection_srs.ImportFromWkt(raster_projection)
-    point_layer = point_vector.CreateLayer(
-        'point_sample', projection_srs, ogr.wkbPoint)
-
-    # wait for vector to reproject
-    task_graph.close()
-    task_graph.join()
-
-    geotransform = raster_info['geotransform']
-    n_cols, n_rows = raster_info['raster_size']
-    inv_geotransform = gdal.InvGeoTransform(geotransform)
-
-    vector = gdal.OpenEx(reprojected_vector_path, gdal.OF_VECTOR)
-    layer = vector.GetLayer()
-    n_features = layer.GetFeatureCount()
-
-    layer_defn = layer.GetLayerDefn()
     original_field_name_list = []
     for i in range(layer_defn.GetFieldCount()):
         point_layer.CreateField(layer_defn.GetFieldDefn(i))
@@ -162,12 +121,65 @@ if __name__ == "__main__":
     point_layer_def = point_layer.GetLayerDefn()
     point_layer.StartTransaction()
 
+    n_written = 0
+    while True:
+        payload = result_queue.get()
+        if payload == 'STOP':
+            print('write results STOP')
+            break
+
+        if n_written % 100 == 0:
+            print(
+                'no less than %.2f%% complete' %
+                (n_written / feature_count * 100.0))
+        n_written += 1
+        feature_id, raster_val_list, point_geom_wkb_list = payload
+        poly_feature = poly_layer.GetFeature(feature_id)
+
+        for raster_val, point_geom_wkb in zip(
+                raster_val_list, point_geom_wkb_list):
+            geom = ogr.CreateGeometryFromWkb(point_geom_wkb)
+            point_feature = ogr.Feature(point_layer_def)
+            point_feature.SetGeometry(geom)
+            point_feature.SetField('rasterval', raster_val)
+            for poly_field_name in original_field_name_list:
+                point_feature.SetField(
+                    poly_field_name,
+                    poly_feature.GetField(poly_field_name))
+            point_layer.CreateFeature(point_feature)
+
+    poly_layer = None
+    vector = None
+    point_layer.CommitTransaction()
+    point_layer = None
+    point_vector = None
+
+
+def poly_test_worker(work_queue, raster_path, poly_vector_path, result_queue):
+
+    raster_info = pygeoprocessing.get_raster_info(raster_path)
+    base_nodata = raster_info['nodata']
+    geotransform = raster_info['geotransform']
+    n_cols, n_rows = raster_info['raster_size']
+    inv_geotransform = gdal.InvGeoTransform(geotransform)
+
     raster = gdal.OpenEx(raster_path, gdal.OF_RASTER | gdal.GA_ReadOnly)
     band = raster.GetRasterBand(1)
 
-    for index, poly_feature in enumerate(layer):
-        if index % 100 == 0:
-            print('%2.f%% complete' % (100*index/n_features))
+    poly_vector = gdal.OpenEx(
+        poly_vector_path, gdal.OF_VECTOR | gdal.GA_ReadOnly)
+    poly_layer = poly_vector.GetLayer()
+
+    while True:
+        payload = work_queue.get()
+        if payload == 'STOP':
+            work_queue.put('STOP')
+            print('poly test STOP')
+            break
+        poly_fid = payload
+
+        poly_feature = poly_layer.GetFeature(poly_fid)
+
         geometry = poly_feature.GetGeometryRef()
         geom_shapely = shapely.wkb.loads(geometry.ExportToWkb())
         geom_prep = shapely.prepared.prep(geom_shapely)
@@ -176,6 +188,8 @@ if __name__ == "__main__":
         x_width = geom_shapely.bounds[2]-geom_shapely.bounds[0]
         y_min = geom_shapely.bounds[1]
         y_width = geom_shapely.bounds[3]-geom_shapely.bounds[1]
+        raster_val_list = []
+        point_geom_wkb_list = []
         while n < 100:
             xp, yp = numpy.random.random(2)
             point_geom = shapely.geometry.Point(
@@ -192,14 +206,143 @@ if __name__ == "__main__":
                 if (numpy.isclose(raster_val, base_nodata) or
                         numpy.isnan(raster_val)):
                     continue
-                print('%f %d %d' % (raster_val, raster_x, raster_y))
-                geom = ogr.CreateGeometryFromWkb(point_geom.wkb)
-                point_feature = ogr.Feature(point_layer_def)
-                point_feature.SetGeometry(geom)
-                point_feature.SetField('rasterval', raster_val)
-                for poly_field_name in original_field_name_list:
-                    point_feature.SetField(
-                        poly_field_name,
-                        poly_feature.GetField(poly_field_name))
-                point_layer.CreateFeature(point_feature)
-    point_layer.CommitTransaction()
+                raster_val_list.append(raster_val)
+                point_geom_wkb_list.append(point_geom.wkb)
+        if raster_val_list:
+            result_queue.put(
+                (poly_fid, raster_val_list, point_geom_wkb_list))
+
+
+if __name__ == "__main__":
+    for dir_path in [WORKSPACE_DIR, CHURN_DIR]:
+        try:
+            os.makedirs(dir_path)
+        except OSError:
+            pass
+
+    start_time = time.time()
+    task_graph = taskgraph.TaskGraph(CHURN_DIR, -1)
+
+    result_queue = multiprocessing.Queue()
+    work_queue = multiprocessing.Queue()
+
+    vector_path = 'data/Exposure4OLU2.shp'
+    raster_path = 'data/olu_00000-0-0-000-000000-00000-00-00000-00_depth.tif'
+    raster_info = pygeoprocessing.get_raster_info(raster_path)
+    raster_projection = raster_info['projection']
+
+    reprojected_vector_path = os.path.join(
+        CHURN_DIR, 'reprojected_' + os.path.basename(vector_path))
+
+    reproject_task = task_graph.add_task(
+        func=pygeoprocessing.reproject_vector,
+        args=(vector_path, raster_projection, reprojected_vector_path),
+        target_path_list=[reprojected_vector_path],
+        task_name='project vector to raster')
+
+    gpkg_driver = ogr.GetDriverByName('GPKG')
+    point_vector_path = os.path.join(CHURN_DIR, 'point_sample.gpkg')
+    point_vector = gpkg_driver.CreateDataSource(
+        point_vector_path)
+    projection_srs = osr.SpatialReference()
+    projection_srs.ImportFromWkt(raster_projection)
+    point_layer = point_vector.CreateLayer(
+        'point_sample', projection_srs, ogr.wkbPoint)
+    point_layer = None
+    point_vector = None
+
+    # wait for vector to reproject
+    task_graph.close()
+    task_graph.join()
+
+    vector = gdal.OpenEx(
+        reprojected_vector_path, gdal.OF_VECTOR | gdal.GA_ReadOnly)
+    layer = vector.GetLayer()
+    feature_count = layer.GetFeatureCount()
+
+    write_worker = multiprocessing.Process(
+        target=write_results,
+        args=(
+            result_queue, feature_count, reprojected_vector_path,
+            point_vector_path))
+    write_worker.start()
+
+    test_worker_list = []
+    for _ in range(multiprocessing.cpu_count()):
+        test_worker_process = multiprocessing.Process(
+            target=poly_test_worker,
+            args=(
+                work_queue, raster_path, reprojected_vector_path,
+                result_queue))
+        test_worker_process.start()
+        test_worker_list.append(test_worker_process)
+
+    for poly_feature in layer:
+        work_queue.put(poly_feature.GetFID())
+    work_queue.put('STOP')
+
+    for test_worker in test_worker_list:
+        test_worker.join()
+    result_queue.put('STOP')
+    write_worker.join()
+
+    print('ran in %.2fs' % (time.time()-start_time))
+
+    # geotransform = raster_info['geotransform']
+    # n_cols, n_rows = raster_info['raster_size']
+    # inv_geotransform = gdal.InvGeoTransform(geotransform)
+
+    # vector = gdal.OpenEx(reprojected_vector_path, gdal.OF_VECTOR)
+    # layer = vector.GetLayer()
+    # n_features = layer.GetFeatureCount()
+
+    # layer_defn = layer.GetLayerDefn()
+    # original_field_name_list = []
+    # for i in range(layer_defn.GetFieldCount()):
+    #     point_layer.CreateField(layer_defn.GetFieldDefn(i))
+    #     original_field_name_list.append(layer_defn.GetFieldDefn(i).GetName())
+    # point_layer.CreateField(ogr.FieldDefn("rasterval", ogr.OFTReal))
+
+    # point_layer_def = point_layer.GetLayerDefn()
+    # point_layer.StartTransaction()
+
+    # raster = gdal.OpenEx(raster_path, gdal.OF_RASTER | gdal.GA_ReadOnly)
+    # band = raster.GetRasterBand(1)
+
+    # for index, poly_feature in enumerate(layer):
+    #     if index % 100 == 0:
+    #         print('%2.f%% complete' % (100*index/n_features))
+    #     geometry = poly_feature.GetGeometryRef()
+    #     geom_shapely = shapely.wkb.loads(geometry.ExportToWkb())
+    #     geom_prep = shapely.prepared.prep(geom_shapely)
+    #     n = 0
+    #     x_min = geom_shapely.bounds[0]
+    #     x_width = geom_shapely.bounds[2]-geom_shapely.bounds[0]
+    #     y_min = geom_shapely.bounds[1]
+    #     y_width = geom_shapely.bounds[3]-geom_shapely.bounds[1]
+    #     while n < 100:
+    #         xp, yp = numpy.random.random(2)
+    #         point_geom = shapely.geometry.Point(
+    #             x_min+x_width*xp, y_min+y_width*yp)
+    #         if geom_prep.intersects(point_geom):
+    #             n += 1
+    #             raster_x, raster_y = [
+    #                 int(v) for v in gdal.ApplyGeoTransform(
+    #                     inv_geotransform, point_geom.x, point_geom.y)]
+    #             if (raster_x < 0 or raster_y < 0 or
+    #                     raster_x >= n_cols or raster_y >= n_rows):
+    #                 continue
+    #             raster_val = band.ReadAsArray(raster_x, raster_y, 1, 1)[0, 0]
+    #             if (numpy.isclose(raster_val, base_nodata) or
+    #                     numpy.isnan(raster_val)):
+    #                 continue
+    #             geom = ogr.CreateGeometryFromWkb(point_geom.wkb)
+    #             point_feature = ogr.Feature(point_layer_def)
+    #             point_feature.SetGeometry(geom)
+    #             point_feature.SetField('rasterval', raster_val)
+    #             for poly_field_name in original_field_name_list:
+    #                 point_feature.SetField(
+    #                     poly_field_name,
+    #                     poly_feature.GetField(poly_field_name))
+    #             point_layer.CreateFeature(point_feature)
+    # point_layer.CommitTransaction()
