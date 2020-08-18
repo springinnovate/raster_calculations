3cb6321b19135d38bbcdb5ae3c6a9a22cd0cd665
becky <rchaplinkramer@gmail.com>
2020-08-18 16:21:08 -0700

    adding back demo_of_percentle.py

---
+++ demo_of_percentile.py
@@ -0,0 +1,57 @@
+"""Demo percentile function."""
+import sys
+import os
+import shutil
+import logging
+import multiprocessing
+
+import pygeoprocessing
+import raster_calculations_core
+from osgeo import gdal
+import taskgraph
+
+gdal.SetCacheMax(2**30)
+
+WORKSPACE_DIR = 'CNC_workspace'
+NCPUS = -1
+try:
+    os.makedirs(WORKSPACE_DIR)
+except OSError:
+    pass
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'),
+    stream=sys.stdout)
+LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    """Write your expression here."""
+
+    path = r"C:\Users\Becky\Documents\carbon_edge_model\error_regression_baccini_biomass.tif"
+    percentile_working_dir = r"C:\Users\Becky\Documents\raster_calculations\percentile_working_dir"
+    #makes a temporary directory because there's a shitton of rasters to find out the percentiles
+    try:
+        os.makedirs(percentile_working_dir)
+    except OSError:
+        pass
+        #checks to see if the directory already exists, if it doesn't it makes it, if it does it doesn't do anything
+    percentile_values_list = pygeoprocessing.raster_band_percentile(
+        #(path, 1), percentile_working_dir, [0, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99, 99.5, 99.9, 99.99, 99.999, 100] )
+        (path, 1), percentile_working_dir, list(range(0, 101, 1)))
+    # (path,1) is indicating the first band in that "path" raster; the 2nd argument is the working dir; the third is the list of percentiles we want
+    shutil.rmtree(percentile_working_dir)
+    #this gets rid of that termporary directory
+    print(percentile_values_list)
+
+   
+
+
+
+
+if __name__ == '__main__':
+    TASK_GRAPH = taskgraph.TaskGraph(WORKSPACE_DIR, NCPUS, 5.0)
+    main()
