38901468b0ac7f7ee5fe959c7a7d229fbba7e789
Rich Sharp <richpsharp@gmail.com>
2020-12-04 16:49:05 -0800

    patching by doing sinusoidal reprojection first

--- raster_calculations_simple.py
+++ raster_calculations_simple.py
@@ -37,19 +37,38 @@ def main():
     """Write your expression here."""
 
     wgs84_srs = osr.SpatialReference()
-    wgs84_srs.ImportFromEPSG(4326)    
+    wgs84_srs.ImportFromEPSG(4326)
+
+    sinusoidal_raster1_path = 'solution_111_tar_80_res_2km_carbon_0.tif'
+    raster_2_path = 'realized_e_source_abs_ann_mean.tif'
+    raster2_info = pygeoprocessing.get_raster_info(raster_2_path)
+    target_pixel_size = (1.495833333333333348, 1.5092592592592593)
+    resample_method = 'average'
+    projected_raster1_path = f'wgs84_projected_{sinusoidal_raster1_path}'
+
+    warp_raster_task = TASK_GRAPH.add_task(
+        func=pygeoprocessing.warp_raster,
+        args=(
+            sinusoidal_raster1_path, target_pixel_size, projected_raster1_path,
+            resample_method),
+        kwargs={
+            'target_projection_wkt': wgs84_srs.ExportToWkt(),
+            'target_bb': raster2_info['bounding_box']},
+        target_path_list=[projected_raster1_path],
+        task_name=f'project {sinusoidal_raster1_path}')
+    warp_raster_task.join()
 
     single_expression = {
         'expression': '(raster2>-9999)*raster1',
         'symbol_to_path_map': {
-            'raster1': r"solution_111_tar_80_res_2km_carbon_0.tif",
-            'raster2': r"realized_e_source_abs_ann_mean.tif"
+            'raster1': projected_raster1_path,
+            'raster2': raster_2_path,
         },
         'target_nodata': -9999,
         'default_nan': -9999,
         'target_projection_wkt': wgs84_srs.ExportToWkt(),
-        'target_pixel_size': (1.495833333333333348,1.5092592592592593),
-        'resample_method': 'average',
+        'target_pixel_size': target_pixel_size,
+        'resample_method': resample_method,
         'target_raster_path': "top80_solution_1.5d_avg.tif",
     }
 
