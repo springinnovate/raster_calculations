ef2df35bdaa9eaa643c1d2f55b4ca311d7113990
Rich Sharp <richpsharp@gmail.com>
2021-06-16 08:56:22 -0700

    adding a loop to extract all bands

--- extract_band.py
+++ extract_band.py
@@ -1,6 +1,10 @@
 from osgeo import gdal
 import pygeoprocessing
 
-pygeoprocessing.raster_calculator(
-    [("baccini_carbon_data_2003_2014_compressed_md5_11d1455ee8f091bf4be12c4f7ff9451b.tif", 12)],
-    lambda x: x, 'baccini_band12.tif', gdal.GDT_Int16, 65535)
+def _passthrough_op(array):
+    return array
+
+for band_id in range(1, 17):
+    pygeoprocessing.raster_calculator(
+        [("baccini_carbon_data_2003_2014_compressed_md5_11d1455ee8f091bf4be12c4f7ff9451b.tif", band_id)],
+        _passthrough_op, f'baccini_band{band_id}.tif', gdal.GDT_Int16, 65535)
