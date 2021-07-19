a6d4fc83fa4dd1df4d3648ad810240c6980273b7
Rich Sharp <richpsharp@gmail.com>
2021-07-19 16:15:38 -0700

    taking out those comments so it does something

--- doit.py
+++ doit.py
@@ -1,7 +1,15 @@
-import gdal
+import os
+import glob
 
-path = "path_to_a_raster"
-raster = gdal.OpenEx(path, gdal.OF_RASTER)
-gtiff_driver = gdal.GetDriverByName("GTiff")
-target_path = 'path_to_target_geotiff.tif'
-gtiff_driver.CreateCopy(target_path, raster)
+import gdal
+import pygeoprocessing
+
+if __name__ == '__main__':
+    gtiff_driver = gdal.GetDriverByName("GTiff")
+    for path in glob.glob('data/*/*/*/*.nc'):
+        print(path)
+        raster_info = pygeoprocessing.get_raster_info(path)
+        print(raster_info['n_bands'])
+        raster = gdal.OpenEx(path, gdal.OF_RASTER)
+        target_path = os.path.join('output', f'{os.path.basename(os.path.splitext(path)[0])}.tif')
+        gtiff_driver.CreateCopy(target_path, raster)
