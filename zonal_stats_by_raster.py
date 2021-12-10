9707173f3f4349d6ce071ec6a04af59274addb3f
Rich Sharp <richpsharp@gmail.com>
2021-12-10 13:24:52 -0800

    refactor to use taskgraph

--- zonal_stats_by_raster.py
+++ zonal_stats_by_raster.py
@@ -9,6 +9,7 @@ import multiprocessing
 
 from osgeo import gdal
 from ecoshard import geoprocessing
+from ecoshard import taskgraph
 import numpy
 
 gdal.SetCacheMax(2**27)
@@ -41,7 +42,9 @@ def mask_out_op(mask_data, base_data, mask_code, base_nodata):
     return result
 
 
-def _calculate_stats(aligned_raster_path_list, mask_code, other_raster_info, mask_raster_path):
+def _calculate_stats(
+        aligned_raster_path_list, mask_code, other_raster_info,
+        mask_raster_path):
     LOGGER.debug(f'_calculate_stats for {mask_raster_path}')
     geoprocessing.raster_calculator(
         [(aligned_raster_path_list[0], 1), (aligned_raster_path_list[1], 1),
@@ -69,16 +72,24 @@ if __name__ == '__main__':
         os.path.splitext(os.path.basename(args.landcover_raster))[0][:40]}_''' + \
         f'{os.path.splitext(os.path.basename(args.other_raster))[0][:12]}'
 
+    task_graph = taskgraph.TaskGraph(
+        working_dir, '.', multiprocessing.cpu_count())
+
     base_raster_path_list = [args.landcover_raster, args.other_raster]
     aligned_raster_path_list = [
         os.path.join(working_dir, os.path.basename(path))
         for path in base_raster_path_list]
     other_raster_info = geoprocessing.get_raster_info(
         args.other_raster)
-    geoprocessing.align_and_resize_raster_stack(
-        base_raster_path_list, aligned_raster_path_list, ['mode', 'near'],
-        other_raster_info['pixel_size'], 'intersection',
-        target_projection_wkt=other_raster_info['projection_wkt'])
+    task_graph.add_task(
+        func=geoprocessing.align_and_resize_raster_stack,
+        args=(
+            base_raster_path_list, aligned_raster_path_list, ['mode', 'near'],
+            other_raster_info['pixel_size'], 'intersection',
+            ),
+        kwargs={'target_projection_wkt': other_raster_info['projection_wkt']},
+        target_path_list=aligned_raster_path_list)
+    task_graph.join()
     lulc_nodata = geoprocessing.get_raster_info(
         args.landcover_raster)['nodata']
     unique_values = get_unique_values(args.landcover_raster)
@@ -87,28 +98,30 @@ if __name__ == '__main__':
     stats_table.write('lucode,min,max,mean,stdev\n')
 
     mask_raster_path_list = []
-    with multiprocessing.Pool() as pool:
-        for mask_code in sorted(unique_values):
-            LOGGER.debug(f'scheduling {mask_code}')
-            mask_raster_path = os.path.join(working_dir, '%d.tif' % mask_code)
-            mask_raster_path_list.append(mask_raster_path)
-            pool.apply_async(
-                _calculate_stats,
-                (aligned_raster_path_list, mask_code, other_raster_info,
-                 mask_raster_path))
-        LOGGER.debug('waiting for it to gadot')
-        pool.close()
-        pool.join()
-        for mask_raster_path in mask_raster_path_list:
-            LOGGER.debug(f'getting stats for {mask_raster_path}')
-            raster = gdal.OpenEx(mask_raster_path, gdal.OF_RASTER)
-            band = raster.GetRasterBand(1)
-            (raster_min, raster_max, raster_mean, raster_stdev) = (
-                band.GetStatistics(0, 1))
-            band = None
-            raster = None
-            stats_table.write(
-                '%d,%f,%f,%f,%f\n' % (
-                    mask_code, raster_min, raster_max, raster_mean, raster_stdev))
+    for mask_code in sorted(unique_values):
+        LOGGER.debug(f'scheduling {mask_code}')
+        mask_raster_path = os.path.join(working_dir, '%d.tif' % mask_code)
+        mask_raster_path_list.append(mask_raster_path)
+        taskgraph.add_task(
+            func=_calculate_stats,
+            args=(
+                aligned_raster_path_list, mask_code, other_raster_info,
+                mask_raster_path),
+            target_path_list=[mask_raster_path],
+            task_name=f'mask {mask_raster_path}')
+    task_graph.close()
+    task_graph.join()
+    LOGGER.debug('waiting for it to gadot')
+    for mask_raster_path in mask_raster_path_list:
+        LOGGER.debug(f'getting stats for {mask_raster_path}')
+        raster = gdal.OpenEx(mask_raster_path, gdal.OF_RASTER)
+        band = raster.GetRasterBand(1)
+        (raster_min, raster_max, raster_mean, raster_stdev) = (
+            band.GetStatistics(0, 1))
+        band = None
+        raster = None
+        stats_table.write(
+            '%d,%f,%f,%f,%f\n' % (
+                mask_code, raster_min, raster_max, raster_mean, raster_stdev))
     stats_table.close()
     shutil.rmtree(working_dir)
