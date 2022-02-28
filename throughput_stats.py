6308777bfc1d7de1243b8d24f9351f857c67e7a6
Rich Sharp <rich@therealspring.org>
2022-02-28 10:44:11 -0800

    just do a reversed test for runtime

--- throughput_stats.py
+++ throughput_stats.py
@@ -6,10 +6,15 @@ from ecoshard import geoprocessing
 
 if __name__ == "__main__":
     path = r"C:\Users\richp\Downloads\impact_obs_10m_2017_compressed_md5_99376e.tif"
-    start_time = time.time()
-    for _ in geoprocessing.iterblocks((path, 1), offset_only=True):
-        pass
-    print(f'took {time.time()-start_time:.2f} seconds to iterblocks with only offset')
-    for _ in geoprocessing.iterblocks((path, 1)):
-        pass
-    print(f'took {time.time()-start_time:.2f} seconds to iterblocks with read')
+    for exp in reversed(range(20, 31)):
+        start_time = time.time()
+        largest_block = 2**exp
+        for _ in geoprocessing.iterblocks(
+                (path, 1), offset_only=True, largest_block=largest_block):
+            pass
+        print(f'offset,{time.time()-start_time:.2f},{exp}')
+        start_time = time.time()
+        for _ in geoprocessing.iterblocks(
+                (path, 1), largest_block=largest_block):
+            pass
+        print(f'iterblocks,{time.time()-start_time:.2f},{exp}')
