0e7a0f39bfbeb10ef19b8b9ff06a0f2fd5778237
Rich Sharp <richpsharp@gmail.com>
2023-03-07 12:45:33 -0800

    don't cog for now

--- subtract_rasters.py
+++ subtract_rasters.py
@@ -40,9 +40,6 @@ def main():
     parser.add_argument('raster_A_path', help='Path to raster A.')
     parser.add_argument('raster_B_path', help='Path to raster B.')
     parser.add_argument('target_raster_path', help='Path to target raster')
-    parser.add_argument(
-        '--cog', action='store_true',
-        help='set if desired output is COG format')
     parser.add_argument('--target_nodata', type=float, help=(
         'If set, use this nodata, otherwise use target nodata value from '
         'raster A'))
@@ -60,19 +57,6 @@ def main():
         [(args.raster_A_path, 1), (args.raster_B_path, 1)],
         subtract_op(a_nodata, b_nodata, target_nodata),
         args.target_raster_path, raster_info['datatype'], target_nodata)
-
-    if args.cog:
-        cog_driver = gdal.GetDriverByName('COG')
-        base_raster = gdal.OpenEx(args.target_raster_path, gdal.OF_RASTER)
-        cog_file_path = os.path.join(
-            f'cog_{os.path.basename(args.target_raster_path)}')
-        options = ('COMPRESS=LZW', 'NUM_THREADS=ALL_CPUS', 'BIGTIFF=YES')
-        LOGGER.info(f'convert {args.target_raster_path} to COG {cog_file_path} with {options}')
-        cog_raster = cog_driver.CreateCopy(
-            cog_file_path, base_raster, options=options,
-            callback=geoprocessing._make_logger_callback(
-                f"COGing {cog_file_path} %.1f%% complete %s"))
-        del cog_raster
 
 
 if __name__ == '__main__':
